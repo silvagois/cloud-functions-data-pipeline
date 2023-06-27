4bfd2b6c38887eb3627b381fa477cd6cbfde504c
silvagois <marcossilvagois@gmail.com>
2023-06-27 17:57:28 -0300

    Creating buckets Terraform and Cloud Function Terraform

--- cloud-function-terraform/terraform/storage.tf
+++ cloud-function-terraform/terraform/storage.tf
@@ -1,29 +1,14 @@
+# Bucket onde sera armazenado a function
 resource "google_storage_bucket" "function_bucket" {
     name     = "${var.project_id}-function"
     location = var.region
+    force_destroy = true
+
 }
 
+# Bucket onde será trigado a function
 resource "google_storage_bucket" "input_bucket" {
     name     = "${var.project_id}-input"
     location = var.region
-}
-
-resource "google_storage_bucket" "landing_bucket"{
-    name = "${var.project_id}-landing"
-    location = var.region
-}
-
-resource "google_storage_bucket" "raw_bucket"{
-    name = "${var.project_id}-raw"
-    location = var.region
-}
-
-resource "google_storage_bucket" "trusted_bucket"{
-    name = "${var.project_id}-trusted"
-    location = var.region
-}
-
-resource "google_storage_bucket" "refined_bucket"{
-    name = "${var.project_id}-refined"
-    location = var.region
+    force_destroy = true
 }
